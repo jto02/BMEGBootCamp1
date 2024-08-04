74431efaf2b8328b16ae596fbdadcbca5894ca25
Jeffrey To <jeffreytoschool@gmail.com>
2024-08-03 22:34:14 -0700

    First Commit! Second Commit!

--- SRC/firstcode/main.java
+++ SRC/firstcode/main.java
@@ -2,14 +2,14 @@ package firstcode;
 
 class main {
     public static void main(String[] args) {
-        int[] boo = {0, 1, 2, 3, 4};
+        int[] lol = {0, 1, 2, 3, 4};
 
         int[] bar = new int[100];
         for(int i = 0; i < 100; i++){
             bar[i] = i;
         }
-        System.out.println("The number of even numbers in boo is:");
-        System.out.println(countEvenNum(boo, 5));
+        System.out.println("The number of even numbers in lol is:");
+        System.out.println(countEvenNum(lol, 5));
 
         System.out.println("The number of even numbers in bar is:");
         System.out.println(countEvenNum(bar, 100));
